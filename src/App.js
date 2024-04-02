fdb5ba574833fc29fbbb41ac72860a704a2f0db1
HalimjonHalimov <88800233+HalimjonHalimov@users.noreply.github.com>
2024-04-02 17:05:39 +0500

    hOME PAGE

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/home'
 import Login from './pages/login'
+import Drainer from './pages/drainer'
 
 function App() {
   const [currentUser, setCurrentUser ] =  useState(true)
@@ -9,10 +10,9 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      {/* <Route path="/" element={currentUser ?  <Home /> : <Login />} />
-      <Route path="/login" element={currentUser ? <Home /> :  <Login />} /> */}
-      <Route path="/" element={ <Home />}/>
-      <Route path="/login" element={<Login />}/>
+      <Route path="/" element={currentUser ?  <Home /> : <Login />} />
+      <Route path="/login" element={currentUser ? <Home /> :  <Login />} />
+      <Route path="/drainer" element={currentUser ? <Drainer /> :  <Login />} />
       <Route path="*" element={<Navigate to={'/'} />} />
     </Routes>
     </BrowserRouter>
